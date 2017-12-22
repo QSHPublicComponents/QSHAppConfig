6f5e8d42488b0892d5649e6a86411b7e54110281
NoryCao <2252055382@qq.com>
2017-12-22 16:25:23 +0800

    2

---
+++ QSHAppConfig/QSHAppConfig.h
@@ -0,0 +1,19 @@
+//
+//  QSHAppConfig.h
+//  QSHAppConfig
+//
+//  Created by caonongyun on 2017/12/22.
+//  Copyright © 2017年 QSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QSHAppConfig.
+FOUNDATION_EXPORT double QSHAppConfigVersionNumber;
+
+//! Project version string for QSHAppConfig.
+FOUNDATION_EXPORT const unsigned char QSHAppConfigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QSHAppConfig/PublicHeader.h>
+
+
